f3c03f55227253f0d2053dd51aa39e6b709979cd
sang1900 <76580837+sang1900@users.noreply.github.com>
2021-12-30 17:03:18 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
+import requests
 
 @Bot.on_message(filters.private & filters.command('files'))
 async def batch(client: Client, message: Message):
